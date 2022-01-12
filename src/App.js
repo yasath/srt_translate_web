d84a4fe47e23fa4a575462e1ec572c91d46c3ec5
yasath <rayasathdias@gmail.com>
2022-01-12 15:09:28 +0000

    Add basic file opening and displaying

--- src/App.js
+++ src/App.js
@@ -1,6 +1,32 @@
+import { useState } from "react";
+
 const App = () => {
+    let fileReader;
+    const [loadedSubtitles, setLoadedSubtitles] = useState("")
+
+    const handleFileRead = (e) => {
+        const content = fileReader.result;
+        setLoadedSubtitles(content);
+    };
+
+    const handleFileChosen = (file) => {
+        fileReader = new FileReader();
+        fileReader.onloadend = handleFileRead;
+        fileReader.readAsText(file);
+    };
+
     return (
-        <h1>srt_translate_web</h1>
+        <div className="container">
+            <h1>srt_translate_web</h1>
+
+            <input
+                type="file"
+                accept=".srt"
+                onChange={e => handleFileChosen(e.target.files[0])}
+            />
+
+            <p>{loadedSubtitles}</p>
+        </div>
     )
 }
 
